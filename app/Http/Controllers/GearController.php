0c5c046ac8e8481bac829156f51982073312edac
Guilherme P. Latuque <guilherme.latuque@gmail.com>
2018-09-16 22:15:10 -0300

    Adding functions to controller class GearController

--- app/Http/Controllers/GearController.php
+++ app/Http/Controllers/GearController.php
@@ -6,5 +6,44 @@ use Illuminate\Http\Request;
 
 class GearController extends Controller
 {
-    //
+    public function index() {
+        $gears = Gear::all();
+        $total = Gear::all()->count();
+        return view('list-gears', compact('gears', 'total'));
+    }
+
+    public function create() {
+        return view('include-gear');
+    }
+
+    public function store(Request $request) {
+        $gear = new Gear;
+        $gear->name = $request->name;
+        $gear->type = $request->type;
+        $gear->save();
+        return redirect()->route('gear.index')->with('message', 'Gear created successfully!');
+    }
+
+    public function show($id) {
+        //
+    }
+
+    public function edit($id) {
+        $gear = Gear::findOrFail($id);
+        return view('alter-gear', compact('gear'));
+    }
+
+    public function update(Request $request, $id) {
+        $gear = Gear::findOrFail($id); 
+        $gear->name = $request->name;
+        $gear->type = $request->type;
+        $gear->save();
+        return redirect()->route('gear.index')->with('message', 'Gear changed successfully!');
+    }
+
+    public function destroy($id) {
+        $gear = Gear::findOrFail($id);
+        $gear->delete();
+        return redirect()->route('gear.index')->with('message', 'Gear deleted successfully!');
+    }
 }
