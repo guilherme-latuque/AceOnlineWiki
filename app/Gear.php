242e50a3ee2970ed4471e5a8af71bc32ca5914a8
Guilherme P. Latuque <guilherme.latuque@gmail.com>
2018-09-16 21:50:55 -0300

    Adding attributes in the model class Gear

--- app/Gear.php
+++ app/Gear.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gear extends Model
 {
-    //
+    protected $fillable = ['name','type'];
+    protected $guarded = ['id', 'created_at', 'update_at'];
+    protected $table = 'gear';
 }
